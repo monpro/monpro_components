4714c21bc8ee3694729860380266c0fd9b25343d
monpro <monproshen@gmail.com>
2020-11-05 10:47:19 +1100

    add icon component

---
+++ src/components/Icon/icon.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+import classNames from 'classnames'
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from '@fortawesome/react-fontawesome'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+
+export type ThemeProps =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'info'
+  | 'warning'
+  | 'danger'
+  | 'light'
+  | 'dark'
+
+export interface IconProps extends FontAwesomeIconProps {
+  theme?: ThemeProps
+}
+
+export const Icon: FC<IconProps> = (props) => {
+  const { className, theme, ...restProps } = props
+  const classes = classNames('mon-icon', className, {
+    [`icon-${theme}`]: theme,
+  })
+  return <FontAwesomeIcon className={classes} {...restProps} />
+}
+
+export default Icon
