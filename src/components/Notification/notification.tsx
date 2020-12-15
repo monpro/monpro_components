59ab96d5cfe1bf68a5b47e52eb1d78b660045ce1
monpro <monproshen@gmail.com>
2020-12-15 18:05:39 +1100

    add more props interface and add notificationInstance

--- src/components/Notification/notification.tsx
+++ src/components/Notification/notification.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-
-
+import Notification from 'rc-notification';
+import { NotificationInstance as RCNotificationInstance } from 'rc-notification/lib/Notification';
 export type NotificationLocation = 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
 
 export type IconType = 'success' | 'error' | 'warning' | 'info'
 
+export interface NotificationProps {
+  message: React.ReactNode;
+  description?: React.ReactNode;
+  btn?: React.ReactNode;
+  key?: string;
+  onClose?: () => void;
+  duration?: number | null;
+  location?: NotificationLocation;
+  style?: React.CSSProperties;
+  prefixClasses?: string;
+  className?: string;
+  onClick?: () => void;
+  top?: number;
+  bottom?: number;
+}
+
+export interface ConfigProps {
+  top?: number;
+  bottom?: number;
+  duration?: number;
+  prefixClasses?: string;
+  // right to left
+  rtl?: boolean;
+}
+
 export interface NotificationInstance {
-  success(args: any):void;
-  error(args: any):void;
-  info(args: any):void;
-  warning(args: any):void;
-  open(args: any):void;
+  success(args: NotificationProps):void;
+  error(args: NotificationProps):void;
+  info(args: NotificationProps):void;
+  warning(args: NotificationProps):void;
+  open(args: NotificationProps):void;
 }
 
 
-
 export interface NotificationApi {
-  warn(args: any): void;
+  warn(args: NotificationProps): void;
   close(key: string): void;
-  config(options: any): void;
+  config(options: ConfigProps): void;
 
   useNotification: () => [NotificationInstance, React.ReactElement]
 }
@@ -29,5 +53,13 @@ const api: any = {
   config: () => {},
   destroy: () => {}
 }
+
+// promise key-value pairs
+const notificationInstance: {
+  [key: string]: Promise<RCNotificationInstance>
+} = {}
+
+
+
 
 export default api as NotificationApi
