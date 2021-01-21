380edd143b35872a468c82ddeff971ab2bcca5be
monpro <monproshen@gmail.com>
2021-01-21 21:04:13 +1100

    add optional attributes for switch toggle component

--- src/components/SwitchToggle/switch.tsx
+++ src/components/SwitchToggle/switch.tsx
@@ -1,21 +1,37 @@
-import {FC, InputHTMLAttributes, ReactElement} from "react";
-import React from "react";
-
-export interface SwitchToggleProps extends InputHTMLAttributes<HTMLElement> {
+import React, {ChangeEvent, FC, InputHTMLAttributes, useState} from "react";
+import classnames from 'classnames';
+export interface SwitchToggleProps extends Omit<InputHTMLAttributes<HTMLElement>, 'onChange'> {
+  id: string;
   name?: string;
+  checked: boolean;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void,
+  optionalLabels?: [string, string],
+  disabled?: boolean;
 }
+
 export const SwitchToggle: FC<SwitchToggleProps> = (props) => {
   const {
-    name
+    id,
+    name,
+    checked,
+    onChange,
+    optionalLabels = ["Y", "N"],
+    disabled= false
   } = props;
+  const switchClasses = classnames("mon-toggle-switch-switch", {
+    "mon-toggle-switch-disabled": disabled
+  })
+
   return (
     <div className="mon-toggle-switch">
       <input
         className="mon-toggle-switch-checkbox"
+        checked={checked}
+        onChange={onChange}
         type="checkbox" name={name} id={name}/>
       <label className="mon-toggle-switch-label" htmlFor={name}>
-        <span className="mon-toggle-switch-inner" yes-text="Y" no-text="N"/>
-        <span className="mon-toggle-switch-switch"/>
+        <span className="mon-toggle-switch-inner" yes-text={optionalLabels[0]} no-text={optionalLabels[1]}/>
+        <span className={switchClasses} />
       </label>
     </div>
 
